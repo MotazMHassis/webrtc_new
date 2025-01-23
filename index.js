0b23461952d3a3a7e29f5816393ee7f9dabe53ae
MotazMHassis <mo3taz_mahmood@yahoo.com>
2025-01-23 06:04:40 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-let port = process.env.PORT || 5000;
-
-let IO = require("socket.io")(port, {
+const port = process.env.PORT || 5000;
+const IO = require("socket.io")(port, {
   cors: {
     origin: "*",
     methods: ["GET", "POST"],
@@ -16,12 +15,16 @@ IO.use((socket, next) => {
 });
 
 IO.on("connection", (socket) => {
-  console.log(socket.user, "Connected");
+  console.log(`User ${socket.user} connected`); // Log when a user connects
+
   socket.join(socket.user);
 
   socket.on("makeCall", (data) => {
     let calleeId = data.calleeId;
     let sdpOffer = data.sdpOffer;
+
+    console.log(`User ${socket.user} is making a call to ${calleeId}`); // Log call offer
+    console.log(`SDP Offer: ${JSON.stringify(sdpOffer)}`); // Log SDP offer
 
     socket.to(calleeId).emit("newCall", {
       callerId: socket.user,
@@ -33,6 +36,9 @@ IO.on("connection", (socket) => {
     let callerId = data.callerId;
     let sdpAnswer = data.sdpAnswer;
 
+    console.log(`User ${socket.user} answered the call from ${callerId}`); // Log call answer
+    console.log(`SDP Answer: ${JSON.stringify(sdpAnswer)}`); // Log SDP answer
+
     socket.to(callerId).emit("callAnswered", {
       callee: socket.user,
       sdpAnswer: sdpAnswer,
@@ -43,9 +49,18 @@ IO.on("connection", (socket) => {
     let calleeId = data.calleeId;
     let iceCandidate = data.iceCandidate;
 
+    console.log(`User ${socket.user} sent ICE candidate to ${calleeId}`); // Log ICE candidate
+    console.log(`ICE Candidate: ${JSON.stringify(iceCandidate)}`); // Log ICE candidate details
+
     socket.to(calleeId).emit("IceCandidate", {
       sender: socket.user,
       iceCandidate: iceCandidate,
     });
   });
+
+  socket.on("disconnect", () => {
+    console.log(`User ${socket.user} disconnected`); // Log when a user disconnects
+  });
 });
+
+console.log(`Server is running on port ${port}`);
