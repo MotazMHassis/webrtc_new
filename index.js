0b92cd027ee961c947dd2a01cbb240815743403e
MotazMHassis <mo3taz_mahmood@yahoo.com>
2025-01-24 04:16:30 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ const IO = require("socket.io")(port, {
   },
 });
 
-const usersInRoom = new Map(); // Map to store users in each room
+const users = new Map(); // Map to store connected users
 
 IO.use((socket, next) => {
   const callerId = socket.handshake.query.callerId;
   if (callerId) {
-    socket.callerId = callerId; // Attach callerId to the socket object
+    socket.callerId = callerId;
     next();
   } else {
     next(new Error("No callerId provided"));
@@ -20,28 +20,56 @@ IO.use((socket, next) => {
 
 IO.on("connection", (socket) => {
   console.log(`User ${socket.callerId} connected`);
+  
+  // Register the user when they connect
+  users.set(socket.callerId, socket.id);
 
   socket.on("makeCall", (data) => {
     const { callerId, calleeId, sdpOffer } = data;
-    console.log(`sdpOffer ${JSON.stringify(sdpOffer)}`);
-    console.log(`User ${callerId} is making a call to ${calleeId}`);
-    socket.to(calleeId).emit("newCall", { callerId, sdpOffer });
+    const calleeSocketId = users.get(calleeId);
+
+    if (calleeSocketId) {
+      console.log(`User ${callerId} is making a call to ${calleeId}`);
+      IO.to(calleeSocketId).emit("newCall", { callerId, sdpOffer });
+      console.log(`SDP Offer: ${JSON.stringify(sdpOffer)}`);
+    } else {
+      console.log(`Callee ${calleeId} not found`);
+      socket.emit("callError", { message: "Callee not available" });
+    }
   });
 
   socket.on("answerCall", (data) => {
     const { callerId, sdpAnswer } = data;
-     console.log(`sdpAnswer ${JSON.stringify(sdpAnswer)}`);
-    console.log(`User ${callerId} answered the call`);
-    socket.to(callerId).emit("callAnswered", { sdpAnswer });
+    const callerSocketId = users.get(callerId);
+
+    if (callerSocketId) {
+      console.log(`User ${socket.callerId} answered the call`);
+      console.log(`SDP Answer: ${JSON.stringify(sdpAnswer)}`);
+      IO.to(callerSocketId).emit("callAnswered", { 
+        callerId: socket.callerId, 
+        sdpAnswer 
+      });
+    } else {
+      console.log(`Caller ${callerId} not found`);
+      socket.emit("callError", { message: "Caller not available" });
+    }
   });
 
   socket.on("IceCandidate", (data) => {
     const { calleeId, iceCandidate } = data;
-    console.log(`Sending ICE candidate to ${calleeId}`);
-    socket.to(calleeId).emit("IceCandidate", { iceCandidate });
+    const calleeSocketId = users.get(calleeId);
+
+    if (calleeSocketId) {
+      console.log(`Sending ICE candidate to ${calleeId}`);
+      IO.to(calleeSocketId).emit("IceCandidate", { 
+        callerId: socket.callerId,
+        iceCandidate 
+      });
+    }
   });
 
   socket.on("disconnect", () => {
+    users.delete(socket.callerId);
     console.log(`User ${socket.callerId} disconnected`);
   });
 });
